3ad0b178ea5430adf79f631d21b063ce859d30f1
Jamie Wallingford <jamie.wallingford@icloud.com>
2019-04-28 22:48:21 -0500

    adding in guidmaker

---
+++ GuidMaker/Controllers/GuidController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuidMaker.Controllers
+{
+    public struct GuidResponse
+    {
+        public Guid guid { get; set; }
+        public string hostname { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    public class GuidController : Controller
+    {
+        public readonly string _machineName;
+
+        public GuidController()
+        {
+            _machineName = Environment.MachineName;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public GuidResponse Get()
+        {
+            return new GuidResponse
+            {
+                guid = Guid.NewGuid(),
+                hostname = _machineName
+            };
+        }
+
+    }
+}
